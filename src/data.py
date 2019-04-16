1f164ac6ecf0b6beae7160f13432159622b0a13b
Locke <>
2019-04-16 16:30:01 -0500

    Prettier axes and legend and colors

--- src/data.py
+++ src/data.py
@@ -1,13 +1,12 @@
 import datetime
 from pprint import pprint
 
+import discord
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from scipy.ndimage.filters import gaussian_filter1d
-
-import discord
 from discord.ext import commands
+from scipy.ndimage.filters import gaussian_filter1d
 
 
 SPACECORD = 391743485616717824
@@ -47,6 +46,8 @@ class Data(commands.Cog):
                     pass
                 else:
                     self.cache[SPACECORD].append((channel.id, channel.name, data))
+        # sort by most total messages first
+        self.cache[SPACECORD] = sorted(self.cache[SPACECORD], key=lambda x: len(x[2]), reverse=True)
         # pprint(self.cache)
         ctx.bot.mydatacache = self.cache
         # await ctx.send(":thumbsup:")
@@ -57,7 +58,7 @@ class Data(commands.Cog):
 
     @commands.command(aliases=['magic'])
     async def graph_data(self, ctx):
-        smoothing = 5
+        smoothing = 10
         if not ctx.bot.mydatacache:
             print("populating cache...")
             cmd = ctx.bot.get_command("get_data")
@@ -67,27 +68,40 @@ class Data(commands.Cog):
             print("cache is already filled")
 
         # Styling
+        fig, ax = plt.subplots(1, 1)
         plt.style.use('ggplot')
-        plt.rcParams['axes.facecolor'] = '#222222'
+        plt.rcParams['legend.frameon'] = False
         plt.rcParams['savefig.facecolor'] = '#222222'
+        plt.rcParams['axes.facecolor'] = '#222222'
         plt.grid(True, 'major', 'x', ls='--', lw=.5, c='w', alpha=.3)
         plt.grid(True, 'major', 'y', ls='--', lw=.5, c='w', alpha=.1)
 
-        # fig, ax = plt.subplots(1, 1)
-        bins = np.linspace(1552719667, 1555398076, 8*31)
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Messages per hour')
+        # ax.set_title('Messages per hour by channel')
+        bins = np.linspace(1552719667, 1555398076, 24*31)
+        maxY = 0
         for channel in ctx.bot.mydatacache[SPACECORD]:
-            if len(channel[2]) > 2:
+            if len(channel[2]) > 5:
                 # convert the epoch format to matplotlib date format
-                # mpl_data = mdates.epoch2num(channel[2])
+                #mpl_data = mdates.epoch2num(channel[2])
                 # plot it
                 y, _, _ = plt.hist(channel[2], bins=bins, alpha=0)
                 ysmoothed = gaussian_filter1d(y, sigma=smoothing)
                 plt.plot(bins[:-1], ysmoothed, label=channel[1])
-        plt.legend(loc='upper right')
+                maxY = max(maxY, max(ysmoothed))
 
+        legend = plt.legend(loc='upper left')
+        plt.setp(legend.get_texts(), color='#888888')
+        plt.ylim(0, maxY*1.05)  # by default it'll scale to show the much higher and invisible bar graph that we don't care about, so manually resize
+        plt.tight_layout()
+
+        #ax.xaxis.set_major_locator(mdates.DayLocator())
+        #ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%h'))
         plt.show()
-        # ax.xaxis.set_major_locator(mdates.DayLocator())
-        # #ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%h'))
+        #fig.savefig('whatever.png', facecolor=fig.get_facecolor(), edgecolor='none')
+
+
 
 
 def setup(bot):
