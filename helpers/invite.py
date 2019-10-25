87bd8a1cc649c2cbc0bb61a25fdce99d24ae824b
Locke <>
2019-10-25 00:43:40 -0500

    delete invites that are posted and bug people accordingly

--- helpers/invite.py
+++ helpers/invite.py
@@ -15,6 +15,7 @@ class InviteWacher(commands.Cog):
 
     async def log(self, invite_code, msg):
         chan = self.bot.get_log_channel(msg.guild.id)
+        changes = self.bot.get_channel(401172732856762370)
         try:
             invite = await self.bot.fetch_invite(invite_code)
         except discord.NotFound:
@@ -30,6 +31,19 @@ class InviteWacher(commands.Cog):
         e.add_field(name='Online members', value=invite.approximate_presence_count)
         e.set_thumbnail(url=invite.guild.icon_url)
         await chan.send(embed=e)
+
+        try:
+            await msg.delete()
+        except discord.Forbidden:
+            await chan.send("(I wouldda deleted it but I was just hiding my powers)")
+        else:
+            await changes.send(f'- deleted an invite by {msg.author} in {msg.channel.mention}')
+
+        staff_bot_room = self.bot.get_channel(395649976048287758)
+        welcome_rules = self.bot.get_channel(391765381645336577)
+        retort = await msg.channel.send(f'Hey {msg.author.mention} please note that the rules here don\'t '
+                                        f'allow posting invites. It may serve you well to review {welcome_rules.mention} again.')
+        await staff_bot_room.send(f'hey yo please warn user {msg.author.id} - {retort.jump_url}')
 
     @commands.Cog.listener()
     async def on_message(self, msg):
